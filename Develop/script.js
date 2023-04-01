c6526c38b47fca015ee7f94a640fc220074a77b7
Sleepy Cat <gilb2966@gmail.com>
2023-03-31 20:46:54 -0500

    started building functions and built remaining variables for password chars

--- Develop/script.js
+++ Develop/script.js
@@ -6,26 +6,7 @@
 var generateBtn = document.querySelector("#generate"); //selecting the html button in JS, then storing this element in generateBtn variable//
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); //Yes ok so listening for the "click" event on the button stored in the generateBtn variable; once this occurs, the writePassword function will run//
-function userInputs() {}
-
-function generatePassword() {
-  fhdjaskllfkdha;
-}
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-//START: start by creating a prompt box on "click" event :)
-window.prompt("Welcome to the random password generator engine"); //object.method not currently attached to anything; we want it to do something. Put it in a function?//
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+//Create arrays for these:
 var specialChars = [
   "!",
   "#",
@@ -60,6 +41,64 @@ var specialChars = [
   "}",
   "~",
 ]; //all OWASP chars except for " .
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [lowerCase.toUpperCase];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//START: start by creating a prompt box on "click" event :)
+//object.method not currently attached to anything; we want it to do something. Put it in a function:
+
+function userInputs() {
+  window.prompt("Welcome to the random password generator engine"); //prompts go in here
+}
+userInputs();
+
+function generatePassword() {
+  fhdjaskllfkdha;
+}
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters// make 4 variables (one for each character type)//
+
+console.log(lowerCase);
+
+console.log(upperCase);
 var randomChar = Math.floor(Math.random() * specialChars.length);
 console.log(randomChar);
 
@@ -75,5 +114,5 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); //selecting the password text input box, then storing this in LOCAL passwordText variable//
 
-  passwordText.value = password; //this is updated the passwordText variable to
+  passwordText.value = password; //this updates the passwordText.value to be the value within the "password" variable, which in turn runs the generatePassword function//
 }
